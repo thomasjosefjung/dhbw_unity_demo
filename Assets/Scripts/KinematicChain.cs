4c99e3e6a4d269c65ee42873cad76c12338917fd
Thomas Jung <mail@thomas-jung.de>
2020-10-25 20:16:42 +0100

    Initial

---
+++ Assets/Scripts/KinematicChain.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class KinematicChain : MonoBehaviour
+{
+    [SerializeField, Range(-180.0f, 180.0f)]
+    private float[] values; 
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var elements = gameObject.GetComponentsInChildren<KinematicJoint>(); 
+        // Array.Reverse(elements); 
+
+        if (values == null || values.Length != elements.Length) {
+            values = new float[elements.Length]; 
+        }
+
+        for (int i=0; i<values.Length; ++i) {
+            elements[i].AngleDeg = values[i]; 
+        }
+    }
+
+    /// <summary>
+    /// Called when the script is loaded or a value is changed in the
+    /// inspector (Called in the editor only).
+    /// </summary>
+    void OnValidate()
+    {
+    }
+}
---
+++ Assets/Scripts/KinematicJoint.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class KinematicJoint : MonoBehaviour
+{
+    [Range(-180.0f, 180.0f)]
+    public float AngleDeg = 0.0f;
+
+    [Range(-45.0f, 45.0f)]
+    public float Speed = 45f;
+
+    public bool KeyboardControl = false; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        current = 0.0f; 
+    }
+
+
+    float current; 
+    float currentVel; 
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (KeyboardControl)
+        {
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                AngleDeg = AngleDeg - Time.deltaTime * Speed;
+            }
+            else if (Input.GetKey(KeyCode.RightArrow))
+            {
+                AngleDeg = AngleDeg + Time.deltaTime * Speed;
+            }
+        }
+
+        // transform.localRotation = Quaternion.AngleAxis(AngleDeg, Vector3.forward);
+        current = Mathf.SmoothDampAngle(current, AngleDeg, ref currentVel, .5f, float.MaxValue);
+        transform.localRotation = Quaternion.AngleAxis(current, Vector3.forward);
+    }
+
+    /// Called when the script is loaded or a value is changed in the
+    /// inspector (Called in the editor only).
+    void OnValidate()
+    {
+        
+    }
+}
